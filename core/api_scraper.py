5255f392bc913b0a9e264e2c76f10f7d1dc12431
aungmyotun-dev <aungmyotun251190@gmail.com>
2026-08-07 13:11:38 +0630

    Add network event debug logs

--- core/api_scraper.py
+++ core/api_scraper.py
@@ -63,6 +63,10 @@ class ApiScraper:
         def handler(response):
 
             nonlocal upcoming
+            logger.info(
+                "RESPONSE %s",
+                response.url,
+            )
 
             url = response.url
 
@@ -89,12 +93,22 @@ class ApiScraper:
                 )
 
         self.page.on("response", handler)
+        def request_handler(request):
+            logger.info("REQUEST %s", request.url)
+
+        def request_finished_handler(request):
+            logger.info("REQUEST FINISHED %s", request.url)
+
+        self.page.on("request", request_handler)
+        self.page.on("requestfinished", request_finished_handler)
 
         try:
+            logger.info("Registering response listener...")
             self.browser.open(BASE_URL)
             self.page.wait_for_timeout(5000)
 
         finally:
+            logger.info("Removing response listener...")
             self.page.remove_listener("response", handler)
 
         if upcoming is None:
@@ -107,6 +121,8 @@ class ApiScraper:
             "Captured %d Match API responses.",
             len(match_candidates),
         )
+        self.page.remove_listener("request", request_handler)
+        self.page.remove_listener("requestfinished", request_finished_handler)
 
         for i, item in enumerate(match_candidates, start=1):
             logger.info(
